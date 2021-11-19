d516fa96461ac1f9ef33f3c799d141f7d6df1b7a
Jayu <jayeshhande00@gmail.com>
2021-11-20 00:42:39 +0530

    translation

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var textinput = document.querySelector("#text-input");
 var btnTranslate = document.querySelector("#btn-translate");
-var outputdiv=  document.querySelector("#output");
+var outputdiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+function getServerURL(text) {
+    return serverURL + "?" + "text=" + text
+}
 
 function clickHandler() {
- outputdiv.
-};
 
+    var inputText = textinput.value;
 
+    fetch(getServerURL(inputText))
+        .then(response => response.json())
+        .then(json =>{
+        var translatedText=(json.contents.translated)
+        outputdiv.innerText=translatedText;})
+        
 
-
+}
 
 btnTranslate.addEventListener("click", clickHandler);
