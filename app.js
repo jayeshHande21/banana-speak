336e659bc9a8294909bfe35316067a7e304a8bab
Jayu <jayeshhande00@gmail.com>
2021-11-19 21:17:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+var textinput = document.querySelector("#text-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputdiv=  document.querySelector("#output");
+
+
 function clickHandler() {
- console.log("texted");
- console.log(textinput.value);
+ outputdiv.
 };
 
 
 
 
-var textinput = document.querySelector("#text-input");
-var btnTranslate = document.querySelector("#btn-translate");
-// var outputdiv=  document.querySelector("#output");
+
 btnTranslate.addEventListener("click", clickHandler);
