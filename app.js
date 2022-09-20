06bf90f49a25a4e08351b21a7108d76b5c582d45
Jayu <jayeshhande00@gmail.com>
2022-09-20 15:41:40 +0530

    feat : add alert

--- app.js
+++ app.js
@@ -2,23 +2,29 @@ var textinput = document.querySelector("#text-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputdiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+var serverURL = "https://api.funtranslations.com/translate/pirate.json";
 
 function getServerURL(text) {
-    return serverURL + "?" + "text=" + text
+  return serverURL + "?" + "text=" + text;
 }
 
+function errorHandler(error) {
+  alert("Something is wrong! Please try after some time.");
+  console.log("Error : " + error);
+}
 function clickHandler() {
+  var inputText = textinput.value;
+  if (inputText === "") {
+    alert("Please enter the Text ");
+  }
 
-    var inputText = textinput.value;
-
-    fetch(getServerURL(inputText))
-        .then(response => response.json())
-        .then(json =>{
-        var translatedText=(json.contents.translated)
-        outputdiv.innerText=translatedText;})
-        
-
+  fetch(getServerURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputdiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickHandler);
