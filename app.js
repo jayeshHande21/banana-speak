afec15e00e1e1432a8fdc0be3d11e491ecf694b5
Jayu <jayeshhande00@gmail.com>
2022-09-20 15:55:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var textinput = document.querySelector("#text-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputdiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/pirate.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getServerURL(text) {
   return serverURL + "?" + "text=" + text;
@@ -16,15 +16,15 @@ function clickHandler() {
   var inputText = textinput.value;
   if (inputText === "") {
     alert("Please enter the Text ");
+  } else {
+    fetch(getServerURL(inputText))
+      .then((response) => response.json())
+      .then((json) => {
+        var translatedText = json.contents.translated;
+        outputdiv.innerText = translatedText;
+      })
+      .catch(errorHandler);
   }
-
-  fetch(getServerURL(inputText))
-    .then((response) => response.json())
-    .then((json) => {
-      var translatedText = json.contents.translated;
-      outputdiv.innerText = translatedText;
-    })
-    .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickHandler);
